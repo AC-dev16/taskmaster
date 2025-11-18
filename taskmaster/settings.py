52c2035d8d839f9bb57a9554a86c7580f115e058
AC-dev16 <amy_mb@hotmail.co.uk>
2025-11-18 11:23:35 +0000

    Add task management functionality with views, templates, and styles

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'taskmaster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+
+def index(request):
+    # Get uncompleted tasks ordered by due date (earliest first)
+    todo_tasks = Task.objects.filter(completed=False).order_by('due_date')
+    
+    # Get completed tasks
+    done_tasks = Task.objects.filter(completed=True).order_by('due_date')
+    
+    context = {
+        'todo_tasks': todo_tasks,
+        'done_tasks': done_tasks,
+    }
+    
+    return render(request, 'tasks/index.html', context)
